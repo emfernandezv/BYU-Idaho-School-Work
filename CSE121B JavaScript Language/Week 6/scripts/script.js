ec6dbe039365470cf6fc6ae122e9734bc32eff8a
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-01-18 01:28:46 -0700

    update

--- CSE121B JavaScript Language/Week 6/scripts/script.js
+++ CSE121B JavaScript Language/Week 6/scripts/script.js
@@ -1,31 +1,28 @@
 let date = null;
 
+// set initial values on the HTML
+function setInitialFields(){
+    document.getElementById('appTitle').innerText = 'MONEY EXCHANGER';
+    document.getElementById('OriginTitle').innerText = 'Origin Currency:';
+    document.getElementById('ExchangeTitle').innerText = 'Final Currency:';
+    document.getElementById('RateTitle').innerText = 'Exchange Rate:';
+    document.getElementById('AmountTitle').innerText = 'Final Amount:';
+    document.getElementById('getrate').innerText = 'Get Rate';
+    document.getElementById('submit').innerText = 'Submit';
+    document.getElementById('message').innerText = '';
+}
+
 // return all the currencies and symbols
 const url1 = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
 async function getSymbol(url1) {
 const response = await fetch(url1);
 if (response.ok) {
-        let originCurrencySymbol = await response.json();
-        let finalCurrencySymbol =  originCurrencySymbol;
-        populateDropDown('originDD',originCurrencySymbol);
-        populateDropDown('finalDD',finalCurrencySymbol);
+        let CurrencySymbolList = await response.json();
+        populateDropDown('originDD',CurrencySymbolList);
+        populateDropDown('exchangeDD',CurrencySymbolList);
     }else{
         document.getElementById('message').innerText = 'There was an API Error trying to reach the first URL!';  
     }
-};
-
-// return all the conversion rate for the selected origin
-async function getExchange(v1,v2) {
-    let url2 = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${v1}.json`;
-    const response = await fetch(url2);
-    if (response.ok) {
-            let valueList = await response.json();
-            date = valueList['date'];
-            let currencyValue = valueList[v1];
-            populateConversion(parseFloat(currencyValue[v2]));
-        } else {
-            document.getElementById('message').innerText = 'There was an API Error trying to reach the second URL!';  
-        }
 };
 
 // sets the dropdowns
@@ -40,10 +37,18 @@ function populateDropDown(name,data) {
     }
 }
 
+//INITIAL LOAD PAGE
+setInitialFields();
+getSymbol(url1) ;
+
+// CLICK EVENTS
+document.getElementById("getrate").addEventListener("click", getRate);
+document.getElementById("submit").addEventListener("click", submit);
+
 // gets the input values and executes the conversion
-function getValue(){
+function getRate(){
     let s1 = document.getElementById('originDD').value
-    let s2 = document.getElementById('finalDD').value
+    let s2 = document.getElementById('exchangeDD').value
     let val = parseFloat(document.getElementById('originValue').value);
     if (s1 == '' || s2 == '' || s1 == null || s2 == null || val <= 0 || isNaN(val)){
         document.getElementById('message').innerText = 'You must input an amount and select the origin/final currency!';  
@@ -51,6 +56,20 @@ function getValue(){
         getExchange(s1,s2);
     }
 }
+
+// return all the conversion rate for the selected origin - dynamic url
+async function getExchange(v1,v2) {
+    let url2 = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${v1}.json`;
+    const response = await fetch(url2);
+    if (response.ok) {
+            let valueList = await response.json();
+            date = valueList['date'];
+            let currencyValue = valueList[v1];
+            populateConversion(parseFloat(currencyValue[v2]));
+        } else {
+            document.getElementById('message').innerText = 'There was an API Error trying to reach the second URL!';  
+        }
+};
 
 // calculates and populares the conversion fields
 function populateConversion(rate){
@@ -61,20 +80,8 @@ function populateConversion(rate){
     document.getElementById('message').innerText = '';
 };
 
-// set initial values on the HTML
-function setInitialFields(){
-    document.getElementById('appTitle').innerText = 'MONEY EXCHANGER';
-    document.getElementById('baseTitle').innerText = 'Origin Currency:';
-    document.getElementById('valueTitle').innerText = 'Final Currency:';
-    document.getElementById('rateTitle').innerText = 'Exchange Rate:';
-    document.getElementById('amountTitle').innerText = 'Final Amount:';
-    document.getElementById('exchange').innerText = 'Get Rate';
-    document.getElementById('apply').innerText = 'Submit';
-    document.getElementById('message').innerText = '';
-}
-
 // displays success message waits 5 secs and resets the page
-function apply(){
+function submit(){
     let val2 = parseFloat(document.getElementById('finalValue').value);
     if ( val2 <= 0 || isNaN(val2)){
         document.getElementById('message').innerText = 'You must get a quote first!';
@@ -88,10 +95,4 @@ function apply(){
 function reset(){
     location.reload();
 }
-
-setInitialFields();
-getSymbol(url1) ;
-
-document.getElementById("exchange").addEventListener("click", getValue);
-document.getElementById("apply").addEventListener("click", apply);
 
