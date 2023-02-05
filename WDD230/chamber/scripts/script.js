5b9fa837c35c3b9593daab95bf3944616943c87f
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-02-04 23:11:30 -0700

    banner

--- WDD230/chamber/scripts/script.js
+++ WDD230/chamber/scripts/script.js
@@ -1,12 +1,21 @@
 //footer date 
 let oLastModif = new Date(document.lastModified).toLocaleDateString('en-EN', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) ;
-document.getElementById('date').innerText = `${oLastModif}`;
+document.getElementById('date').innerText = `Last Update:  ${oLastModif}`;
 
 //foother paragraph
 const d = new Date();
 let year = d.getFullYear();
 let oLastModif2 = new Date(document.lastModified).toLocaleDateString('en-us') ;
 document.querySelector('#info').innerText = `©${year}    |    Eduardo Fernandez    |    WDD 230 Project    |    Last Modification: ${oLastModif2}`;
+
+let day = d.getDay();
+let banner = document.querySelector('.banner')
+
+if (d.getDay() == 1 || d.getDay() == 2){
+    banner.style.display = 'block'
+}else {
+    banner.style.display = 'none'
+}
 
 const mainnav = document.getElementById('nav-menu')
 const hambutton = document.getElementById('menu');
