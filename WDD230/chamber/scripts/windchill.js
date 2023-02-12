997478c4abc5f1f6b4f6ee5b5b1ededbc12777f2
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-02-11 23:48:45 -0700

    W6

---
+++ WDD230/chamber/scripts/windchill.js
@@ -0,0 +1,11 @@
+let t = document.getElementById("grade").innerText
+let s = document.getElementById("windspeed").innerText
+
+if (t <= 50 && s >3 ){
+    let factor = 35.74 + 0.215 * t - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
+    document.getElementById("windchill").innerText = factor.toFixed(2)
+}else{
+    document.getElementById("windchill").innerText = "N/A"
+}
+
+
