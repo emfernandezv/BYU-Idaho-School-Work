2a2d867613dec5198fad395d7ae8d58eae1ca096
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-02-25 18:14:56 -0700

    counter fixed

--- WDD230/chamber/scripts/counter.js
+++ WDD230/chamber/scripts/counter.js
@@ -1,16 +1,29 @@
 const visitsDisplay = document.querySelector("#visits");
-var LastVisit = window.localStorage.getItem("LastVisit-ls");
-var localeDateTime = new Date();
-if (LastVisit == undefined){
-    LastVisit = localeDateTime.getTime() ;
-}
-if (LastVisit  !== localeDateTime.getTime() ) {
-    let difference = localeDateTime.getTime() - LastVisit;
-    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    var display = 'Last time you visited this site was: '+TotalDays+" day(s) ago.";
-}else{
-    var display = `This is your first visit!`;
-}
-visitsDisplay.innerHTML = display;
 
-localStorage.setItem("LastVisit-ls",LastVisit)
+// Get the current date
+const nowDate = new Date();
+
+// Get the date of the last visit from local storage
+const lastVisit = localStorage.getItem('lastVisit');
+
+if (lastVisit) {
+  // Calculate the difference in time between the last visit and the current visit
+  const diffTime = nowDate.getTime() - new Date(lastVisit).getTime();
+
+  // Convert the time difference to days and round to the nearest whole number
+  const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+
+  // Display the number of days since the last visit
+  if (diffDays < 1){
+    visitsDisplay.innerText = `Last visit: Today`;
+
+  } else{
+    visitsDisplay.innerText = `Last visit: ${diffDays} day(s) ago`;
+    
+  }
+} else{
+    visitsDisplay.innerText = "This is your first visit"
+}
+
+// Store the current visit date in local storage
+localStorage.setItem('lastVisit', nowDate);
