9b9454dd5fd0b08757f1588649218e4776213ddf
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-02-25 17:56:29 -0700

    W7 fix

--- WDD230/chamber/scripts/counter.js
+++ WDD230/chamber/scripts/counter.js
@@ -1,11 +1,16 @@
 const visitsDisplay = document.querySelector("#visits");
-var numVisits = window.localStorage.getItem("visits-ls");
-
-if (numVisits !== 0) {
-    visitsDisplay.innerHTML = numVisits;
-}else{
-    visitsDisplay.innerHTML = `This is your first visit!`;
+var LastVisit = window.localStorage.getItem("LastVisit-ls");
+var localeDateTime = new Date();
+if (LastVisit == undefined){
+    LastVisit = localeDateTime.getTime() ;
 }
+if (LastVisit  !== localeDateTime.getTime() ) {
+    let difference = localeDateTime.getTime() - LastVisit;
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    var display = 'Last time you visited this site was: '+TotalDays+" day(s) ago.";
+}else{
+    var display = `This is your first visit!`;
+}
+visitsDisplay.innerHTML = display;
 
-numVisits++;
-localStorage.setItem("visits-ls",numVisits)
+localStorage.setItem("LastVisit-ls",LastVisit)
