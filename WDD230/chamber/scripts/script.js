0db0007b688f0692da1177e15e4b9d19ec8fecfa
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-03-11 17:51:46 -0700

    Chamber weather api

--- WDD230/chamber/scripts/script.js
+++ WDD230/chamber/scripts/script.js
@@ -1,3 +1,25 @@
+const visitsDisplay = document.querySelector("#visits");
+// Get the current date
+const nowDate = new Date();
+// Get the date of the last visit from local storage
+const lastVisit = localStorage.getItem('lastVisit');
+if (lastVisit) {
+  // Calculate the difference in time between the last visit and the current visit
+  const diffTime = nowDate.getTime() - new Date(lastVisit).getTime();
+  // Convert the time difference to days and round to the nearest whole number
+  const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+  // Display the number of days since the last visit
+  if (diffDays < 1){
+    visitsDisplay.innerText = `Last visit: Today`;
+  } else{
+    visitsDisplay.innerText = `Last visit: ${diffDays} day(s) ago`;
+  }
+} else{
+    visitsDisplay.innerText = "This is your first visit"
+}
+// Store the current visit date in local storage
+localStorage.setItem('lastVisit', nowDate);
+
 
 //footer date 
 let oLastModif = new Date(document.lastModified).toLocaleDateString('en-EN', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) ;
@@ -17,7 +39,6 @@ if (d.getDay() == 1 || d.getDay() == 2){
 }else {
     banner.style.display = 'none'
 }
-
 const mainnav = document.getElementById('nav-menu')
 const hambutton = document.getElementById('menu');
 
@@ -25,5 +46,4 @@ const hambutton = document.getElementById('menu');
 // When clicked, the <ul class="navigation">'s class list toggle'
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 var localeDateTime = new Date();
-document.getElementById('datetime').innertext = localeDateTime;
-
+document.getElementById('datetime').innerText = localeDateTime;
--- WDD230/chamber/scripts/windchill.js
+++ WDD230/chamber/scripts/windchill.js
@@ -1,9 +1,53 @@
-let t = document.getElementById("grade").innerText
-let s = document.getElementById("windspeed").innerText
+    // select HTML elements in the document
+const currentTemp = document.getElementById('grade');
+const weatherIcon = document.getElementById('weather-icon');
+const captionDesc = document.getElementById('weathertext');
+const windSpeed = document.getElementById("windspeed")
 
-if (t <= 50 && s >3 ){
-    let factor = 35.74 + 0.6215 * t - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
-    document.getElementById("windchill").innerText = factor.toFixed(1)
-}else{
-    document.getElementById("windchill").innerText = "N/A"
+const url =   "https://api.openweathermap.org/data/2.5/weather?q=kirkland&appid=df316398a3c9e783d731138fffc20584&units=imperial"
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
 }
+  
+apiFetch();
+
+function  displayResults(weatherData) {
+    //Getting the current temp
+    const temp = weatherData.main.temp.toFixed(0);
+    //getting the icon
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    // getting the description
+    const desc = weatherData.weather[0].description;
+    //getting the wind speed
+    const wind = weatherData.wind.speed.toFixed(0);
+
+    //setting the temp
+    currentTemp.innerHTML = `<strong>${temp}</strong>`;
+    //setting the icon
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc.toUpperCase());
+    //setting descripcion
+    captionDesc.textContent = desc.toUpperCase();
+    //setting wind speed
+    windSpeed.innerText = wind;
+    //calculating & the wind chell
+    if (temp <= 50 && windSpeed > 3 ){
+        let factor = 35.74 + 0.6215 * temp - (35.75 * Math.pow(windSpeed,0.16)) + (0.4275 * temp * Math.pow(windSpeed,0.16))
+        document.getElementById("windchill").innerText = factor.toFixed(1)
+    }else{
+        document.getElementById("windchill").innerText = "N/A"
+    }
+}
+
+
