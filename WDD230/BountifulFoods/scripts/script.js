c76cec9a07e40c1e74f6fa2f35a5233323e8f704
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-03-26 00:00:34 -0600

    header and footer

---
+++ WDD230/BountifulFoods/scripts/script.js
@@ -0,0 +1,19 @@
+const mainnav = document.getElementById('nav-menu')
+const hambutton = document.getElementById('menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+    if (hambutton.textContent != "≡ Menu"){
+        hambutton.textContent = "≡ Menu";
+    }else{
+        hambutton.textContent = "X Close";
+    }
+}, false);
+
+
+
+//footer date 
+let oLastModif = new Date(document.lastModified).toLocaleDateString('en-EN', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) ;
+document.getElementById('update').innerText = `Last Update:  ${oLastModif}`;
